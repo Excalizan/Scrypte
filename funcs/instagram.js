685fd1155f2dec4a49b49f946063731e1b24b816
Excalizan <aydinkement24@gmail.com>
2023-08-15 16:44:42 +0300

    a little more error handling, just in case

--- funcs/instagram.js
+++ funcs/instagram.js
@@ -3,6 +3,7 @@ const ig = require('instagram-url-dl')
 function downloadFromInstagram(bot, chatId, url) {
 	ig(url)
 		.then((res) => {
+			bot.sendMessage(chatId, 'Downloading...')
 			// if there are multiple images/videos
 			if (res.data.length > 1) {
 				res.data.forEach((item) => {
@@ -13,21 +14,29 @@ function downloadFromInstagram(bot, chatId, url) {
 						})
 					} else if (item.type === 'video') {
 						bot.sendVideo(chatId, item.url).catch((err) => {
-							bot.sendMessage(chatId, 'Error sending vidoe')
+							bot.sendMessage(chatId, 'Error sending video')
 							console.log(err)
 						})
 					}
 				})
 			} else {
 				if (res.data[0].type === 'image') {
-					bot.sendPhoto(chatId, res.data[0].url)
+					bot.sendPhoto(chatId, res.data[0].url).catch((err) => {
+						bot.sendMessage(chatId, 'Error sending photo')
+						console.log(err)
+					})
 				} else if (res.data[0].type === 'video') {
-					bot.sendVideo(chatId, res.data[0].url)
+					bot.sendVideo(chatId, res.data[0].url).catch((err) => {
+						bot.sendMessage(chatId, 'Error sending video')
+						console.log(err)
+					})
 				}
 			}
 		})
 		.catch((err) => {
-			bot.sendMessage(chatId, 'Error: ' + err.message)
+			bot.sendMessage(chatId, 'Error: ' + err.message).catch((err) => {
+				console.log(err)
+			})
 			console.log(err)
 		})
 }
