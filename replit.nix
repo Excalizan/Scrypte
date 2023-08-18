dfa36c8d90b4266ad4d0a42fb48849fc790118a0
Excalizan <77782958+Excalizan@users.noreply.github.com>
2023-08-18 10:07:54 +0000

    ffmpeg must be one of the most f'd up tools ever created

--- replit.nix
+++ replit.nix
@@ -1,8 +1,9 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
+        pkgs.nodejs-18_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.replitPackages.jest
+        pkgs.ffmpeg
 	];
 }
