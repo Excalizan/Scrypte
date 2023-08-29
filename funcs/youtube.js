7a0d8d065cbca8c0fcf8186cbeca040a90e39c71
Excalizan <ozankement34@gmail.com>
2023-08-29 23:15:18 +0300

    isnt this much of err handling enough already?

--- funcs/youtube.js
+++ funcs/youtube.js
@@ -4,49 +4,54 @@ const ytdl = require('ytdl-core')
 async function downloadFromYoutube(bot, chatId, url) {
 	try {
 		// Get video information and thumbnail URL
-		const videoInfo = await ytdl.getInfo(url)
+		const videoInfo = await ytdl.getInfo(url).catch((err) => {
+			bot
+				.sendMessage(chatId, 'Error getting video information.')
+				.catch((err) => {
+					console.log(err)
+				})
+			console.log(err)
+		})
+
 		const title = videoInfo.player_response.videoDetails.title
 		const thumbnailUrl =
 			videoInfo.videoDetails.thumbnails[
 				videoInfo.videoDetails.thumbnails.length - 1
 			].url
 		// Send a message to show the download progress
-		const message = await bot
+		await bot
 			.sendMessage(chatId, `*Downloading video:* ${title}`)
 			.catch((err) => {
 				console.log(err)
 			})
 
 		// Create a writable stream to store the video file
-		const writeStream = fs.createWriteStream(
-			`content/${title}-${chatId}.mp4`
-		)
+		const writeStream = fs.createWriteStream(`content/${title}-${chatId}.mp4`)
 
 		// Start the download and pipe the video data to the writable stream
 		ytdl(url, { filter: 'audioandvideo' }).pipe(writeStream)
 
 		// When the download is complete, send the video and delete the file
 		writeStream.on('finish', () => {
-			bot.sendVideo(
-				chatId,
-				`content/${title}-${chatId}.mp4`,
-				{
-					caption: `*Video downloaded:* ${title} "by" @Excalizan`,
-					thumb: thumbnailUrl,
-					duration: videoInfo.videoDetails.lengthSeconds,
-					parse_mode: 'Markdown',
-				},
-				{
-					filename: `content/${title}-${chatId}.mp4`,
-					contentType: 'video/mp4',
-				}
-			)
+			bot
+				.sendVideo(
+					chatId,
+					`content/${title}-${chatId}.mp4`,
+					{
+						caption: `*Video downloaded:* ${title} "by" @Excalizan`,
+						thumb: thumbnailUrl,
+						duration: videoInfo.videoDetails.lengthSeconds,
+						parse_mode: 'Markdown',
+					},
+					{
+						filename: `content/${title}-${chatId}.mp4`,
+						contentType: 'video/mp4',
+					}
+				)
 				.catch((err) => {
-					bot.sendMessage(chatId, 'Error sending video.').catch(
-						(err) => {
-							console.log(err)
-						}
-					)
+					bot.sendMessage(chatId, 'Error sending video.').catch((err) => {
+						console.log(err)
+					})
 					console.log(err)
 				})
 
@@ -58,11 +63,9 @@ async function downloadFromYoutube(bot, chatId, url) {
 					}
 				})
 				.catch((error) => {
-					bot.sendMessage(chatId, 'Error sending video.').catch(
-						(err) => {
-							console.log(err)
-						}
-					)
+					bot.sendMessage(chatId, 'Error sending video.').catch((err) => {
+						console.log(err)
+					})
 					console.error(error)
 				})
 		})
